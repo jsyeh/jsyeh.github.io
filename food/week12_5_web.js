f37846baba24ba17620ab3297951ef0c000ed157
Jeng-Sheng Yeh 葉正聖 <jsyeh@mail.mcu.edu.tw>
2022-05-20 09:36:27 +0800

    Update week12_5_web.js

--- food/week12_5_web.js
+++ food/week12_5_web.js
@@ -21,14 +21,20 @@ function draw() {
         rect(50, 100, 150, 2);
     if (choice == 2)
         rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
     text("中餐吃泡麵", 50, 50);
     text("中餐UberEat", 50, 100);
     text("中餐FoodPanda", 50, 150);
+    text("中餐吃牛排", 50, 200);
+    text("中餐吃大餐", 50, 250);
 }
 
 function mousePressed() {
     // choice = (choice + 1) % 3;
-    choice = int(random(3));
+    choice = int(random(5));
 }
 
 function initializeFields() {
