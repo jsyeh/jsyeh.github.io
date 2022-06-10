e81c7321286edc0dfba480abb4997e15698ed739
Jeng-Sheng Yeh 葉正聖 <jsyeh@mail.mcu.edu.tw>
2022-06-10 11:57:16 +0800

    add go 五子棋

--- go/go.js
+++ go/go.js
@@ -1,42 +1,38 @@
 
+// 要如何把程式放上網? (0) Processing Java寫完程式
+// (1) 把程式copy到 pde2js.herokuapp.com, 再轉換
 function setup() {
     initializeFields();
-    createCanvas(450, 450);
+    createCanvas(500, 500);
 }
 
-// 在 Java 的陣列
+// 棋子的座標 陣列
 var bx;
 
-// 和C/C++/C#不同
+// 棋子的座標 陣列
 var by;
 
-// 現在有 N 的棋子
+// 棋子的數字,一開始0個棋子
 var N;
 
 function draw() {
-    background(color(0xF5, 0xD6, 0x65));
-    for (var y = 50; y < 450; y += 50) {
-        line(0, y, 450, y);
+    background(color(0xFF, 0xFF, 0xF2));
+    for (var y = 50; y < 500; y += 50) {
+        line(0, y, 500, y);
     }
-    for (var x = 50; x < 450; x += 50) {
-        line(x, 0, x, 450);
+    for (var x = 50; x < 500; x += 50) {
+        line(x, 0, x, 500);
     }
     for (var i = 0; i < N; i++) {
-        if (// 2倍數,黑
-        i % 2 == 0)
-            // 2倍數,黑
+        if (i % 2 == 0)
             fill(0);
         else
-            // 白
             fill(255);
         ellipse(bx[i], by[i], 40, 40);
     }
-    if (// 2倍數,黑
-    N % 2 == 0)
-        // 2倍數,黑
+    if (N % 2 == 0)
         fill(0);
     else
-        // 白
         fill(255);
     ellipse(mouseX, mouseY, 40, 40);
 }
